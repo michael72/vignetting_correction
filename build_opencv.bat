e4271d6e329ac52132d088083c981ab1015d20d4
Michael Schulte <michaelschu@miltenyibiotec.de>
2020-10-24 22:59:58 +0200

    improve build with open cv

--- build_opencv.bat
+++ build_opencv.bat
@@ -1,6 +1,8 @@
 if not exist build mkdir build
 cd build
+set USE_OPENCV=1
 conan install ..\conanfile_opencv.txt --settings arch=x86_64
 cmake -E env CXXFLAGS="-DUSE_OPENCV" cmake .. -DCMAKE_GENERATOR_PLATFORM=x64 
 cmake --build . --config Release
+cd ..
 
